ce1060283fda6fc7454677d2f796e4f7fb91e683
kirill1991okt <kirill1991okt@gmail.com>
2021-09-07 09:08:40 +0300

    fix: Homework8

--- Homework8/script8.js
+++ Homework8/script8.js
@@ -1,18 +1,20 @@
 // Задание 1
 var arr = [-1, 0, 2, 34, -2];
 
-var newArr = arr.filter(function (item) {
-  return item > 0;
-});
+function filterNumbersArr(array) {
+  return array.filter(function (item) {
+    return item > 0;
+  });
+}
 
-alert(newArr);
+filterNumbersArr(arr);
 
 // Задание 2
 
 var arr = [-1, 0, 2, 34, -2];
 
-function positiveNumber(arr) {
-  return arr.find(function (item) {
+function positiveNumber(array) {
+  return array.find(function (item) {
     return item > 0;
   });
 }
@@ -22,8 +24,7 @@ positiveNumber(arr);
 // Задание 3
 
 function isPalindrome(word) {
-  var wordReverse = word.split('').reverse().join('');
-  return word.toLowerCase() === wordReverse.toLowerCase() ? true : false;
+  return word.toLowerCase() === word.split('').reverse().join('').toLowerCase();
 }
 
 isPalindrome('шалаШ');
@@ -34,12 +35,7 @@ isPalindrome('казак');
 // Задание 4
 
 function areAnagrams(word1, word2) {
-  if (word1.length !== word2.length) {
-    return false;
-  } else if (word1.split('').sort().join() !== word2.split('').sort().join()) {
-    return false;
-  }
-  return true;
+  return word1.split('').sort().join() === word2.split('').sort().join();
 }
 
 areAnagrams('кот', 'отк');
