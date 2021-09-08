db30f4692a3f46a38889190296ff6d2b1b0ad2a7
kirill1991okt <kirill1991okt@gmail.com>
2021-09-08 17:01:51 +0300

    fix: Homework10

--- Homework10/script.js
+++ Homework10/script.js
@@ -10,4 +10,14 @@ cellBtn.addEventListener('click', function () {
   tbody.insertBefore(row, tbody.children[0]);
 });
 
-console.log(tbody.children[0]);
+table.addEventListener('click', function (event) {
+  //   console.log(event);
+  console.dir(event.target);
+  if (event.target.tagName === 'TD' && event.target.textContent === '') {
+    event.target.innerHTML = '<input type="text">';
+    event.target.innerHTML.autofocus = true;
+    console.log(event.target.innerHTML);
+  }
+});
+
+// tbody.lastElementChild.firstElementChild.getAttribute('id');
