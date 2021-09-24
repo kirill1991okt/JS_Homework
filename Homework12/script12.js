f1e98bc48e6b393147e8231b553bea2f1947d029
kirill1991okt <kirill1991okt@gmail.com>
2021-09-24 13:48:35 +0300

    fix: Homework12

--- Homework12/script12.js
+++ Homework12/script12.js
@@ -1,73 +1,87 @@
 var btn = document.getElementById('btn');
-
 var wrapper = document.getElementsByClassName('wrapper')[0];
 
 var content = document.createElement('div');
-content.classList.add('content-tabs')
+content.classList.add('content-tabs');
 wrapper.appendChild(content);
 
 var description = document.createElement('div');
 description.classList.add('description');
 wrapper.appendChild(description);
 
+btn.addEventListener('click', clickHandler);
 
+function clickHandler() {
+  var xhr = new XMLHttpRequest();
 
-btn.addEventListener('click', function () {
-    var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://reqres.in/api/users?page=2', true);
 
-    xhr.open('GET', 'https://reqres.in/api/users?page=2', true);
+  xhr.send();
 
-    xhr.send();
+  xhr.onload = function () {
+    if (this.status === 200) {
+      var response = JSON.parse(this.response).data;
 
-    xhr.onload = function () {
-        if (this.status === 200) {
+      localStorage.data = JSON.stringify(response);
 
-            var response = JSON.parse(this.response).data;
+      for (var i = 0; i < response.length; i++) {
+        var users = document.createElement('div');
+        // debugger;
+        users.classList.add('users');
+        users.innerHTML = 'Пользователь ' + (i + 1);
+        content.appendChild(users);
+      }
 
-            localStorage.data = JSON.stringify(response);
+      for (var j in response) {
+        var descriptionUser = document.createElement('div');
 
-            for (var i = 0; i <= response.length; i++) {
+        descriptionUser.classList.add('description-user');
 
-                var users = document.createElement('div');
+        descriptionUser.innerHTML =
+          '<img src="' +
+          response[j].avatar +
+          '"><div><p>First name: ' +
+          response[j].first_name +
+          '</p><p>Last name: ' +
+          response[j].last_name +
+          '</p></div></img>';
+        description.appendChild(descriptionUser);
+      }
 
-                content.appendChild(users);
-                users.classList.add('users');
-                users.innerHTML = 'Пользователь ' + (i + 1);
+      description.children[0].classList.add('active');
+      content.children[0].classList.add('active');
+
+      btn.removeEventListener('click', clickHandler);
+      //   btn.disabled = true; // может можно по другому сделать
+
+      var usersTabs = document.getElementsByClassName('users');
+      var userDescription = document.getElementsByClassName('description-user');
+
+      for (var i = 0; i < userDescription.length; i++) {
+        userDescription[i].setAttribute('id', 'tab_' + [i]);
+        usersTabs[i].dataset.tab = 'tab_' + [i];
+      }
+
+      for (var i = 0; i < usersTabs.length; i++) {
+        usersTabs[i].addEventListener('click', function () {
+          if (!this.classList.contains('active')) {
+            var tabId = this.getAttribute('data-tab');
+            console.log(tabId);
+            var currentTab = document.getElementById(tabId);
+            console.log(currentTab);
+            for (var j = 0; j < usersTabs.length; j++) {
+              usersTabs[j].classList.remove('active');
             }
 
-
-            for (var j in response) {
-
-                var descriptionUser = document.createElement('div');
-
-                descriptionUser.classList.add('description-user');
-
-                descriptionUser.innerHTML =
-                    '<img src="' +
-                    response[j].avatar +
-                    '"><div><p>First name: ' + response[j].first_name + '</p><p>Last name: ' + response[j].last_name + '</p></div></img>';
-                description.appendChild(descriptionUser);
-
-                console.log(description.children);
+            for (var j = 0; j < userDescription.length; j++) {
+              userDescription[j].classList.remove('active');
             }
 
-            description.children[0].classList.add('active');
-            content.children[0].classList.add('active');
-
-
-
-            btn.disabled = true; // может можно по другому сделать
-        }
-
-        wrapper.addEventListener('click', function (event) {
-            var usersElements = document.getElementsByClassName('users');
-
-            if (event.target.className === 'users') {
-                for (var i = 0; i < usersElements.length; i++) {
-                    usersElements[i].classList.remove('active');
-                }
-                event.target.classList.add('active');
-            }
+            this.classList.add('active');
+            currentTab.classList.add('active');
+          }
         });
-    };
-});
+      }
+    }
+  };
+}
