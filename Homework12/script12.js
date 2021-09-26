ab45d3f8ab0db0d4f26b26931831b5946dd1f90d
kirill1991okt <kirill1991okt@gmail.com>
2021-09-26 19:34:13 +0300

    finish: Homework12

--- Homework12/script12.js
+++ Homework12/script12.js
@@ -12,79 +12,116 @@ wrapper.appendChild(description);
 btn.addEventListener('click', clickHandler);
 
 function clickHandler() {
-  var xhr = new XMLHttpRequest();
 
-  xhr.open('GET', 'https://reqres.in/api/users?page=2', true);
+    if (localStorage.data) {
 
-  xhr.send();
+        var dataParse = JSON.parse(localStorage.data);
 
-  xhr.onload = function () {
-    if (this.status === 200) {
-      var response = JSON.parse(this.response).data;
+        createUsersAndDescriptionUser(dataParse);
 
-      localStorage.data = JSON.stringify(response);
+        description.children[0].classList.add('active');
+        content.children[0].classList.add('active');
 
-      createUsersAndDescriptionUser(response);
+        btn.removeEventListener('click', clickHandler);
 
-      description.children[0].classList.add('active');
-      content.children[0].classList.add('active');
+        var usersTabs = document.getElementsByClassName('users');
+        var userDescription = document.getElementsByClassName('description-user');
 
-      btn.removeEventListener('click', clickHandler);
+        createTabsAndDescription(usersTabs, userDescription);
 
-      var usersTabs = document.getElementsByClassName('users');
-      var userDescription = document.getElementsByClassName('description-user');
 
-      createTabsAndDescription(usersTabs, userDescription);
+    } else {
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'https://reqres.in/api/users?page=2', true);
+
+        xhr.send();
+
+        xhr.onload = function () {
+
+            var currentStatus = +String(this.status)[0];
+
+            if (currentStatus === 2 || currentStatus === 3) {
+                var response = JSON.parse(this.response).data;
+
+                localStorage.data = JSON.stringify(response);
+
+                createUsersAndDescriptionUser(response);
+
+                description.children[0].classList.add('active');
+                content.children[0].classList.add('active');
+
+                btn.removeEventListener('click', clickHandler);
+
+                var usersTabs = document.getElementsByClassName('users');
+                var userDescription = document.getElementsByClassName('description-user');
+
+                createTabsAndDescription(usersTabs, userDescription);
+            } else {
+                console.log(this.status + ': ' + xhr.statusText);
+                var blockError = document.createElement('div');
+                blockError.classList.add('error');
+                blockError.innerHTML = 'Ошибка ' + this.status + ': ' + this.statusText;
+                description.appendChild(blockError);
+                btn.style.display = 'none';
+            }
+        };
+
+        xhr.onloadend = function () {
+            console.log('Загрузка завершена');
+        };
+
     }
-  };
+
 }
 
 function createUsersAndDescriptionUser(response) {
-  for (var i = 0; i < response.length; i++) {
-    var users = document.createElement('div');
+    for (var i = 0; i < response.length; i++) {
+        var users = document.createElement('div');
 
-    users.classList.add('users');
-    users.innerHTML = 'Пользователь ' + (i + 1);
-    content.appendChild(users);
-  }
+        users.classList.add('users');
+        users.innerHTML = 'Пользователь ' + (i + 1);
+        content.appendChild(users);
+    }
 
-  for (var j in response) {
-    var descriptionUser = document.createElement('div');
-    descriptionUser.classList.add('description-user');
+    for (var j in response) {
+        var descriptionUser = document.createElement('div');
+        descriptionUser.classList.add('description-user');
 
-    descriptionUser.innerHTML =
-      '<img src="' +
-      response[j].avatar +
-      '"><div><p>First name: ' +
-      response[j].first_name +
-      '</p><p>Last name: ' +
-      response[j].last_name +
-      '</p></div></img>';
-    description.appendChild(descriptionUser);
-  }
+        descriptionUser.innerHTML =
+            '<img src="' +
+            response[j].avatar +
+            '"><div><p>First name: ' +
+            response[j].first_name +
+            '</p><p>Last name: ' +
+            response[j].last_name +
+            '</p></div></img>';
+        description.appendChild(descriptionUser);
+    }
 }
 
 function createTabsAndDescription(tabs, description) {
-  for (var i = 0; i < tabs.length; i++) {
-    description[i].setAttribute('id', 'tab_' + [i]);
-    tabs[i].dataset.tab = 'tab_' + [i];
-    tabs[i].addEventListener('click', function () {
-      if (!this.classList.contains('active')) {
-        var tabId = this.getAttribute('data-tab');
-        console.log(tabId);
-        var currentTab = document.getElementById(tabId);
-        console.log(currentTab);
-        for (var j = 0; j < tabs.length; j++) {
-          tabs[j].classList.remove('active');
-        }
+    for (var i = 0; i < tabs.length; i++) {
+        description[i].setAttribute('id', 'tab_' + [i]);
+        tabs[i].dataset.tab = 'tab_' + [i];
+        tabs[i].addEventListener('click', function () {
+            if (!this.classList.contains('active')) {
+                var tabId = this.getAttribute('data-tab');
 
-        for (var j = 0; j < description.length; j++) {
-          description[j].classList.remove('active');
-        }
+                var currentTab = document.getElementById(tabId);
 
-        this.classList.add('active');
-        currentTab.classList.add('active');
-      }
-    });
-  }
+                for (var j = 0; j < tabs.length; j++) {
+                    tabs[j].classList.remove('active');
+                }
+
+                for (var j = 0; j < description.length; j++) {
+                    description[j].classList.remove('active');
+                }
+
+                this.classList.add('active');
+                currentTab.classList.add('active');
+            }
+        });
+    }
 }
