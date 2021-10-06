4fcea45182ef181e06cdfbaabdd17ed0fac9034f
kirill1991okt <kirill1991okt@gmail.com>
2021-10-06 10:46:42 +0300

    finish: Homework13

--- Homework13/script13.js
+++ Homework13/script13.js
@@ -1,5 +1,42 @@
-// Задача 1
+// Задание 1
 
-var reg = /^([A-Za-z]{3,10}\_[A-Za-z]{3,10})(\-[0-9]{0,4})?@[\-?\.?0-9A-Za-z]{2,20}\.com$/;
+var reg =
+  /^([A-Za-z]{3,10}\_[A-Za-z]{3,10})(\-[0-9]{0,4})?@[\-?\.?0-9A-Za-z]{2,20}\.com$/;
 
 reg.test('name_surname-1234@gmail.com');
+
+// Задание 2
+
+// Вариант 1
+function validationPhone(number) {
+  var regExp =
+    /^((\+?(375))|(8(\-)?0))\-?(17|44|29|25|33)\-?[1-9]{3}\-?[0-9]{2}\-?[0-9]{2}$/;
+  var phone = number.match(regExp);
+  if (phone) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+// Вариант 2
+
+function validationPhone(number) {
+  return /^((\+?(375))|(8(\-)?0))\-?(17|44|29|25|33)\-?[1-9]{3}\-?[0-9]{2}\-?[0-9]{2}$/.test(
+    number
+  );
+}
+
+// Задание 3
+
+//Вариант 1
+function countVowel(str) {
+  var vowel = str.match(/[a,e,i,o,u,y]/gi);
+  return vowel === null ? 0 : vowel.length;
+}
+
+// Вариант 2
+function countVowel(str) {
+  var newStr = str.replace(/[^a,e,i,o,u,y]/gi, '');
+  return newStr.length;
+}
