9d49e7b7e349214ed1e0cee7715958b717c63a93
kirill1991okt <kirill1991okt@gmail.com>
2021-11-10 22:57:30 +0300

    start: HomeProject

---
+++ HomewProject/api/database.js
@@ -0,0 +1,82 @@
+[{  
+    id: 'TASD110.1AC1',
+    fluid: 0,
+    cooling_capacity: 385,
+    power: 123,
+    current: 219,
+    water_temperature: 7,
+    outdoor_temperature: 35
+},
+{  
+    id: 'TASD145.1AC1',
+    fluid: 0,
+    cooling_capacity: 505,
+    power: 159,
+    current: 288,
+    water_temperature: 7,
+    outdoor_temperature: 35
+},
+{  
+    id: 'TASD170.1AC1',
+    fluid: 0,
+    cooling_capacity: 601,
+    power: 189,
+    current: 341,
+    water_temperature: 7,
+    outdoor_temperature: 35
+},
+{  
+    id: 'TASD210.1AC1',
+    fluid: 0,
+    cooling_capacity: 730,
+    power: 233,
+    current: 419,
+    water_temperature: 7,
+    outdoor_temperature: 35
+},
+{  
+    id: 'TASD230.2AC1',
+    fluid: 0,
+    cooling_capacity: 808,
+    power: 254,
+    current: 479,
+    water_temperature: 7,
+    outdoor_temperature: 35
+},
+{  
+    id: 'TASD260.2AC1',
+    fluid: 0,
+    cooling_capacity: 909,
+    power: 285,
+    current: 507,
+    water_temperature: 7,
+    outdoor_temperature: 35
+},
+{  
+    id: 'TASD285.2AC1',
+    fluid: 0,
+    cooling_capacity: 1001,
+    power: 319,
+    current: 578,
+    water_temperature: 7,
+    outdoor_temperature: 35
+},
+{  
+    id: 'TASD345.2AC1',
+    fluid: 0,
+    cooling_capacity: 1210,
+    power: 379,
+    current: 690,
+    water_temperature: 7,
+    outdoor_temperature: 35
+},
+{  
+    id: 'TASD405.2AC1',
+    fluid: 0,
+    cooling_capacity: 1425,
+    power: 3464,
+    current: 838,
+    water_temperature: 7,
+    outdoor_temperature: 35
+}
+]
